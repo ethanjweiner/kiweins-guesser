c52f424edd9c752b3f71cd5dca92e6e453e6e1af
Ethan Weiner <ethanweiner20@gmail.com>
2024-06-12 23:46:40 -0400

    update skeleton

--- app/games/components/suspense/QuizAnswersSkeleton.tsx.tsx
+++ app/games/components/suspense/QuizAnswersSkeleton.tsx.tsx
@@ -1,5 +1,5 @@
 import Skeleton from "react-loading-skeleton";
 
 export default function QuizAnswersSkeleton() {
-  return <Skeleton count={4} height={48} className="mt-4 rounded" />;
+  return <Skeleton count={4} height={32} className="mt-2 rounded" />;
 }
